c8a87b1c910044b20416dd4e9d7f4de8ed646f48
Brad Chao <brad@brad.tw>
2016-11-28 14:27:20 +0800

    v1

---
+++ app/src/main/java/com/example/user/brad19/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.user.brad19;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void b1(View v){
+        Intent intent =
+                new Intent(MediaStore.Audio.Media.RECORD_SOUND_ACTION);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+            Log.v("brad", uri.getPath());
+        }
+    }
+}
